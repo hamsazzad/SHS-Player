aa8060d48771da7463e24a91cfd1d5e739757638
hamsazzad <shobujkhanshs520@gmail.com>
2026-03-11 03:17:41 +0600

    fix: add core:database dependency to feature:videopicker for FavoriteDao

--- feature/videopicker/build.gradle.kts
+++ feature/videopicker/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(project(":core:common"))
     implementation(project(":core:data"))
     implementation(project(":core:domain"))
+    implementation(project(":core:database"))
     implementation(project(":core:media"))
     implementation(project(":core:model"))
 

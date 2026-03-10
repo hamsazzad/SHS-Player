07f1859b4d8f8953762ab06dd1396b89cb452e65
hamsazzad <shobujkhanshs520@gmail.com>
2026-03-10 20:15:09 +0600

    Fix crash: remove nested verticalScroll in PlayerMenuView; Add real music scanner with MediaStore.Audio

--- feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/ui/PlayerMenuView.kt
+++ feature/player/src/main/java/dev/anilbeesetti/nextplayer/feature/player/ui/PlayerMenuView.kt
@@ -6,12 +6,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -42,7 +39,6 @@ fun PlayerMenuView(
 ) {
     Column(
         modifier = modifier
-            .verticalScroll(rememberScrollState())
             .padding(bottom = 24.dp),
     ) {
         MenuItem(
